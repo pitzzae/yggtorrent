ed002fcf76e57109b5d91c02a667b8b48001a93e
Guillaume Torresani <gtorresa@student.42.fr>
2019-04-24 10:17:37 +0200

    Fix get_info on wrong torrent id

--- parsing/get_info.js
+++ parsing/get_info.js
@@ -3,5 +3,9 @@ const cheerio = require('cheerio');
 exports.get_info = function function_name(data, callback)
 {
 	var dom = cheerio.load(data.toString('utf8'));
-	callback(dom('div[class="description-header"]').next().html().trim().replace(/<\/[a-z]+>/g, '$&\n'));
+	let html_info = dom('div[class="description-header"]').next().html();
+	if (html_info)
+		callback(html_info.trim().replace(/<\/[a-z]+>/g, '$&\n'));
+	else
+		callback(data.toString('utf8'));
 }
