4ea4a6ce11d1745bbfb3b58d8b39ed0bb25425a2
Guillaume Torresani <g.torresani@mesdocteurs.com>
2020-04-14 11:14:08 +0200

    Fix cloudflare

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const request = require('request');
+var cloudscraper = require('cloudscraper');
+var cookie = require('cookie');
+
 const uri_action = {
 	get_categories: {
 		method: 'GET',
@@ -68,17 +71,16 @@ const uri_action = {
 	}
 };
 
-function Client(API_HOST) {
+function Client (API_HOST) {
 	this.credential = {
 		user: null,
 		password: null,
 		cookies: []
 	};
-	fetch_url.API_HOST = API_HOST || 'www2.yggtorrent.pe';
+	fetch_url.API_HOST = API_HOST || 'www2.yggtorrent.gg';
 }
 
-Client.prototype.set_credential = function(user, password, API_HOST)
-{
+Client.prototype.set_credential = function (user, password, API_HOST) {
 	let sync = true;
 	fetch_url.credential = {
 		user: user,
@@ -87,41 +89,35 @@ Client.prototype.set_credential = function(user, password, API_HOST)
 		yggp_: null
 	};
 	fetch_cf_clearance(`https://${fetch_url.API_HOST}${uri_action.search.path}`)
-	.then(() => {
-		this.get('get_categories', '', (data) => {
-			fetch_url.categories_list = {};
-			for (var i = 0; i < data.length; i++)
-			{
-				if (data[i].cats)
-				{
-					for (var j = 0; j < data[i].cats.length; j++)
-					{
-						if (parseInt(data[i].cats[j].value) == data[i].cats[j].value)
-							fetch_url.categories_list[data[i].cats[j].value] = data[i].cats[j]['data-categorie'];
+		.then(() => {
+			this.get('get_categories', '', (data) => {
+				fetch_url.categories_list = {};
+				for (var i = 0; i < data.length; i++) {
+					if (data[i].cats) {
+						for (var j = 0; j < data[i].cats.length; j++) {
+							if (parseInt(data[i].cats[j].value) == data[i].cats[j].value)
+								fetch_url.categories_list[data[i].cats[j].value] = data[i].cats[j]['data-categorie'];
+						}
 					}
 				}
-			}
-			sync = false;
+				sync = false;
+			});
+		})
+		.catch(err => {
+			console.log(err);
 		});
-	})
-	.catch(err => {
-		console.log(err);
-	});
-	while(sync) {require('deasync').sleep(100);}
-    return;
-}
+	while (sync) {require('deasync').sleep(100);}
+	return;
+};
 
-Client.prototype.get = function(action, query, callback)
-{
-	if(typeof query === 'function')
-	{
+Client.prototype.get = function (action, query, callback) {
+	if (typeof query === 'function') {
 		callback = query;
 		query = '';
 	}
-	if(uri_action[action])
+	if (uri_action[action])
 		fetch_url(action, query, uri_action[action].parsing, null, callback);
-	else
-	{
+	else {
 		console.log(`Wrong action, action: ${action}, query: ${query}`);
 		if (callback)
 			callback(null);
@@ -130,111 +126,89 @@ Client.prototype.get = function(action, query, callback)
 	}
 };
 
-Client.prototype.get_categories = function(callback)
-{
+Client.prototype.get_categories = function (callback) {
 	this.get('get_categories', '', callback);
-}
+};
 
-Client.prototype.get_nfo = function(callback, id)
-{
+Client.prototype.get_nfo = function (callback, id) {
 	this.get('get_nfo', 'torrent=' + id, callback);
-}
+};
 
-Client.prototype.get_info = function(callback, id)
-{
+Client.prototype.get_info = function (callback, id) {
 	this.get('get_info', id + '-x', callback);
-}
+};
 
-Client.prototype.search = function(callback, search, category, subcategory, page)
-{
-    var category_tmp = category ? category : 'all';
-    var subcategory_tmp = subcategory ? subcategory : 'all';
-    var page_tmp = parseInt(page) > 0 ? (parseInt(page) * 50) - 50 : '0';
-    this.get('search', 'category=' + category_tmp + '&sub_category=' + subcategory_tmp + '&do=search&name=' + encodeURIComponent(search) + '&page=' + page_tmp, callback);
-}
+Client.prototype.search = function (callback, search, category, subcategory, page) {
+	var category_tmp = category ? category : 'all';
+	var subcategory_tmp = subcategory ? subcategory : 'all';
+	var page_tmp = parseInt(page) > 0 ? (parseInt(page) * 50) - 50 : '0';
+	this.get('search', 'category=' + category_tmp + '&sub_category=' + subcategory_tmp + '&do=search&name=' + encodeURIComponent(search) + '&page=' + page_tmp, callback);
+};
 
-Client.prototype.get_top_day = function(callback)
-{
+Client.prototype.get_top_day = function (callback) {
 	this.get('get_top_day', '', callback);
-}
+};
 
-Client.prototype.get_top_week = function(callback)
-{
+Client.prototype.get_top_week = function (callback) {
 	this.get('get_top_week', '', callback);
-}
+};
 
-Client.prototype.get_top_month = function(callback)
-{
+Client.prototype.get_top_month = function (callback) {
 	this.get('get_top_month', '', callback);
-}
+};
 
-Client.prototype.get_mostseeded = function(callback)
-{
+Client.prototype.get_mostseeded = function (callback) {
 	this.get('get_mostseeded', '', callback);
-}
+};
 
-Client.prototype.get_mostcompleted = function(callback)
-{
+Client.prototype.get_mostcompleted = function (callback) {
 	this.get('get_mostcompleted', '', callback);
-}
+};
 
-Client.prototype.get_torrent = function(callback, id)
-{
+Client.prototype.get_torrent = function (callback, id) {
 	this.get('get_torrent', 'id=' + id, callback);
-}
+};
 
-async function fetch_cf_clearance(url) {
+async function fetch_cf_clearance (url) {
 	return new Promise((resolve) => {
-		const phantom = require('phantom');
-		phantom.create().then(instance => {
-			instance.createPage().then(page => {
-				let count = 0;
-				page.setting('userAgent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0')
-				page.on('onResourceRequested', function(requestData) {
-					count++;
+		cloudscraper.get({
+			uri: url,
+			headers: {
+				'User-Agent': 'Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36',
+				'Cache-Control': 'private',
+				'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5'
+			},
+			callback: (a, b, c) => {
+				fetch_url.credential.cookies = cookie.parse(b.request.headers.cookie);
+				b.request.response.headers['set-cookie'].forEach(c => {
+					const ctmp = cookie.parse(c);
+					Object.keys(ctmp).map(function (key) {
+						if (key !== 'expires' && key !== 'Max-Age' && key !== 'path'  && key !== 'domain') {
+							fetch_url.credential.cookies[key] = ctmp[key]
+						}
+						return null;
+					});
 				});
-				page.on('onLoadFinished', function(requestData) {
-					if (count > 1)
-					{
-						page.property('cookies').then(cookies => {
-							instance.exit();
-							fetch_url.credential.cookies = [];
-							cookies.forEach(e => {
-								fetch_url.credential.cookies.push(`${e.name}=${e.value}`);
-							});
-							resolve();
-							/*
-							cookies.forEach(e => {
-								if (e.name === 'cf_clearance')
-								{
-									let timeout = (e.expiry * 1000) - (new Date().getTime());
-									fetch_url.credential.cf_clearance = e.value;
-									fetch_url.credential.cf_clearance_expiry = e.expiry;
-									setTimeout(() => {
-										fetch_cf_clearance(url);
-									}, timeout - 180000);
-								}
-							});
-							*/
-						});
-					}
-				});
-				page.open(url);
-			});
+				resolve();
+			}
 		});
+
 	});
 };
 
-function fetch_url(action, query, parsing, auth, callback)
-{
+function fetch_url (action, query, parsing, auth, callback) {
+	const cookieObject = fetch_url.credential.cookies;
+	const cookies = Object.keys(cookieObject).map(function (key) {
+		return cookie.serialize(key, cookieObject[key]);
+	});
 	const options = {
 		url: `https://${fetch_url.API_HOST}${uri_action[action].path}${(uri_action[action].query_params ? query : '')}`,
 		method: uri_action[action].method,
 		headers: {
 			'Host': fetch_url.API_HOST,
 			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0',
-			'Cookie': `${fetch_url.credential.cookies.join('; ')}`
+			'User-Agent': 'Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36',
+			'Cookie': `${cookies.join('; ')}`
 		},
 		encoding: null
 	};
@@ -243,44 +217,35 @@ function fetch_url(action, query, parsing, auth, callback)
 			id: this.fetch_url.credential.user,
 			pass: this.fetch_url.credential.password
 		};
-	else
-	{
+	else {
 		options.body = uri_action[action].query_params ? '' : query;
-		options.headers['Content-Length'] = Buffer.byteLength(uri_action[action].query_params ? '' : query, 'utf8')
+		options.headers['Content-Length'] = Buffer.byteLength(uri_action[action].query_params ? '' : query, 'utf8');
 	}
-	request.post(options, function optionalCallback(err, res, buffer) {
-		if (res.statusCode === 301)
-		{
+	request.post(options, function optionalCallback (err, res, buffer) {
+		console.log(`${options.method} - ${res.statusCode} - ${options.url}`);
+		if (res.statusCode === 301) {
 			var split_location = res.headers.location.split('/');
 			fetch_url.API_HOST = split_location[2];
 			fetch_url(action, query, parsing, auth, callback);
-		}
-		else if (res.statusCode === 307)
-		{
-			if (res.headers['set-cookie'])
-			{
+		} else if (res.statusCode === 307) {
+			if (res.headers['set-cookie']) {
 				res.headers['set-cookie'].forEach(c => {
 					let yggp;
-					if (c.match(/ygg/g) && (yggp_ = c.match(/=[a-z0-9]+/)) && yggp_ && yggp_[0])
-					{
+					if (c.match(/ygg/g) && (yggp_ = c.match(/=[a-z0-9]+/)) && yggp_ && yggp_[0]) {
 						fetch_url.credential.yggp_ = yggp_[0].replace('=', '');
 					}
 				});
 			}
 			fetch_url(action, query, parsing, auth, callback);
-		}
-		else if (res.statusCode === 503)
-		{
+		} else if (res.statusCode === 503) {
 			fetch_cf_clearance(`https://${fetch_url.API_HOST}${uri_action.search.path}`)
-			.then(() => {
-				fetch_url(action, query, parsing, auth, callback)
-			})
-			.catch(err => {
-				fetch_url(action, query, parsing, auth, callback)
-			});
-		}
-		else
-		{
+				.then(() => {
+					fetch_url(action, query, parsing, auth, callback);
+				})
+				.catch(err => {
+					fetch_url(action, query, parsing, auth, callback);
+				});
+		} else {
 			if (action == 'get_torrent')
 				parsing(buffer, callback, {
 					uri_action: uri_action,
