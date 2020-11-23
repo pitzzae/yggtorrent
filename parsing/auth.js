a098de4f8d02726c9dbb9e6273e7fb54a9606a44
Guillaume Torresani <g.torresani@mesdocteurs.com>
2020-11-23 15:34:09 +0100

    Change repo

--- parsing/auth.js
+++
@@ -1,9 +0,0 @@
-exports.auth = function function_name(data, callback, auth_obj)
-{
-	var string_data = data.toString('utf8');
-	auth_obj.fetch_url.credential.count++;
-	if (string_data == '' && auth_obj.fetch_url.credential.count < 5)
-		auth_obj.fetch_url('get_torrent', auth_obj.query, auth_obj.uri_action['get_torrent'].parsing, auth_obj, callback);
-	else
-		callback(null, "Incorrect identifier");
-}
